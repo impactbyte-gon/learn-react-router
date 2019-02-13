2608aa64175d78ef0b7a48127e66e8202c22b273
ajinbramantio <ajinbramantio@gmail.com>
2019-02-13 16:25:39 +0700

    ajin aja

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Help from './pages/Help'
+import Ajin from './pages/Ajin'
 
 class App extends Component {
   render() {
@@ -11,15 +12,17 @@ class App extends Component {
       <Router>
         <div>
           <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/help">Help</Link>
+            <Link to="/">Home </Link>
+            <Link to="/about">About </Link>
+            <Link to="/help">Help </Link>
+            <Link to="/ajin">Ajin </Link>
           </nav>
 
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/help" component={Help} />
+            <Route path="/ajin" component={Ajin} />
           </Switch>
         </div>
       </Router>
---
+++ src/pages/Ajin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Ajin extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ajin</h1>
+        <p>i'm tired.</p>
+      </div>
+    )
+  }
+}
+
+export default Ajin
