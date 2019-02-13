a84cb0be89cb7e46c403d6cc930919831ce9bd49
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-13 16:08:38 +0700

    Use React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Help from './pages/Help'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/help">Help</Link>
+          </nav>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/help" component={Help} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/pages/Help.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Help = () => {
+  return (
+    <div>
+      <h1>Help</h1>
+      <p>Please let us help you.</p>
+    </div>
+  )
+}
+
+export default Help
