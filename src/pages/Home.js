49b6456bc4f9280cd2ca60c5ec53c5fd82c82247
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-13 16:29:13 +0700

    Put link to Ari page in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <p>Welcome to my website!</p>
+      <p>
+        Visit <Link to="/ari">Ari</Link> page
+      </p>
     </div>
   )
 }
