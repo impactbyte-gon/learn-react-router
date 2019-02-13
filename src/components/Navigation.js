8d92f1f9538e67a6fe111725541eed361ab852e9
Mario Rizki <mariorizki07@gmail.com>
2019-02-13 16:30:48 +0700

    add link to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const StyledNav = styled.nav`
   a {
     text-decoration: none;
     margin-right: 10px;
   }
-`
+`;
 
 const Navigation = () => {
   return (
@@ -15,8 +15,9 @@ const Navigation = () => {
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/help">Help</Link>
+      <Link to="/mario">Mario</Link>
     </StyledNav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
