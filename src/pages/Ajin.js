a9e794f51c9a60a24829483e5af9715ff3c985e6
ajinbramantio <ajinbramantio@gmail.com>
2019-02-13 16:34:23 +0700

    Update branch ajin

--- src/pages/Ajin.js
+++ src/pages/Ajin.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 class Ajin extends Component {
+  constructor() {
+    super()
+    this.state = {
+      Firstname: 'Fadhila',
+      LastName: 'Fajrin'
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Ajin</h1>
+        <h1>
+          {this.state.Firstname} {this.state.LastName}
+        </h1>
         <p>i'm tired.</p>
       </div>
     )
