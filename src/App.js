21dabb058232ef2498efa14ef016f938dbc552ad
Muhammad Fahrianda <mfahrianda@gmail.com>
2019-02-13 16:24:12 +0700

    Add ari page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Help from './pages/Help'
+import Ari from './pages/Ari'
 
 class App extends Component {
   render() {
@@ -14,12 +15,14 @@ class App extends Component {
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/help">Help</Link>
+            <Link to="/ari">Ari</Link>
           </nav>
 
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/help" component={Help} />
+            <Route path="/ari" component={Ari} />
           </Switch>
         </div>
       </Router>
---
+++ src/pages/Ari.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Ari = () => {
+  return (
+    <div>
+      <h1>Ari</h1>
+      <p>Student at Impact Byte</p>
+    </div>
+  )
+}
+
+export default Ari
